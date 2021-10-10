db6f0fd9f0660c22ca99a82247920642b4a5a084
Naphatsara <Naphatsara43@gmail.com>
2021-10-10 20:50:06 +0700

    homework flutter food 2

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food/pages/home/home_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoginPage extends StatefulWidget {
   static const routeName = '/login';
+
   const LoginPage({Key? key}) : super(key: key);
 
   @override
@@ -10,8 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  var pin = '123456';
   var input = '';
-  var add = 0;
 
   void _showMaterialDialog(String title, String msg) {
     showDialog(
@@ -84,17 +87,27 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         for (var i = 0; i < input.length; i++)
-                          Icon(
-                            Icons.circle,
-                            size: 30.0,
-                            color: Colors.amber,
+                          Container(
+                            margin: EdgeInsets.all(4.0),
+                            width: 24.0,
+                            height: 24.0,
+                            decoration: BoxDecoration(
+                                color: Theme.of(context).colorScheme.primary,
+                                shape: BoxShape.circle),
                           ),
-                        for (var i = 0; i < 6 - input.length; i++)
-                          Icon(
-                            Icons.circle_outlined,
-                            size: 30.0,
-                            color: Colors.amber,
-                          )
+                        for (var i = input.length; i < 6; i++)
+                          Container(
+                            margin: EdgeInsets.all(4.0),
+                            width: 24.0,
+                            height: 24.0,
+                            decoration: BoxDecoration(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .primary
+                                  .withOpacity(0.2),
+                              shape: BoxShape.circle,
+                            ),
+                          ),
                       ],
                     ),
                   ],
@@ -136,19 +149,32 @@ class _LoginPageState extends State<LoginPage> {
       }
     });
     setState(() {
-      if (input.length == 6) {
-        if (input == '123456') {
-          /*Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => Homepage()),
-          )*/
-          Navigator.pushReplacementNamed(context, HomePage.routeName); //ตัวแปรสีม่วง = instance of class , สีขาว = local
-        } else {
-          _showMaterialDialog('ERROR', 'Invalid PIN. Please try again');
-          input = '';
-        }
+      if (input.length == pin.length) {
+        _test(input);
       }
     });
+  }
+
+  Future<void> _test(String input) async {
+    var url = Uri.parse('https://cpsu-test-api.herokuapp.com/login');
+    var response = await http.post(url, body: {'pin': input});
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonBody = json.decode(response.body);
+      String status = jsonBody['status'];
+      String? message = jsonBody['message'];
+      bool data = jsonBody['data'];
+
+      print('STATUS: $status');
+      print('MESSAGE: $message');
+      print('DATA: $data');
+
+      if (data == true) {
+        Navigator.pushReplacementNamed(context, HomePage.routeName);
+      } else {
+        _showMaterialDialog('ERROR', 'Invalid PIN. Please try again');
+        input = '';
+      }
+    }
   }
 }
 
@@ -177,7 +203,11 @@ class LoginButton extends StatelessWidget {
         decoration: number == -2
             ? null
             : BoxDecoration(
-                shape: BoxShape.circle, border: Border.all(width: 2.0)),
+                shape: BoxShape.circle,
+                border: Border.all(
+                  width: 2.0,
+                ),
+              ),
         child: Center(
           child: number >= 0
               ? Text('$number', style: Theme.of(context).textTheme.headline6)
@@ -192,6 +222,3 @@ class LoginButton extends StatelessWidget {
     );
   }
 }
-
-
-
